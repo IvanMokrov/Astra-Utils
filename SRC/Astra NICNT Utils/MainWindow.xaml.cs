fd9ecd0cc6021e2909a4eae943faab72b343b7ae
Ivan Mokrov <ivan.mk@hotmail.com>
2016-10-08 02:47:52 +0300

    Minor changes in usings

--- SRC/Astra NICNT Utils/MainWindow.xaml.cs
+++ SRC/Astra NICNT Utils/MainWindow.xaml.cs
@@ -1,21 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Astra_NICNT_Utils.Annotations;
 using Astra_NICNT_Utils.ViewModel;
 
 namespace Astra_NICNT_Utils
@@ -42,17 +26,10 @@ namespace Astra_NICNT_Utils
             txtLog.ScrollToEnd();
         }
 
+        // Too simple for ICommand, no interaction
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-            AddLog("Mabuta1 asdf asdf as.ld,fj a;sdf asdhf laksdjfh aslkdjfh laksdjfh aslkdf hlaskdfh laksdhfj alskdfh laskdfh jlaskdhf laskdf hlaskdjfh alskdfh alskdfh alskdhf j");
-            AddLog("Not yet implemened");
-            AddLog("Not yet implemened");
-            AddLog("Not yet implemened");
         }
 
         // Too simple for ICommand, no interaction
